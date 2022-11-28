ae1804df2356bd0aa047ea490491076d20b56381
novakvova <novakvova@gmail>
2022-11-28 05:21:23 +0200

    Working Castle

---
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; // Ссылка на интересующий обьект
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    [Header("Set Dynamically")]
+    public float camZ; // Желаемая координата Z камеры
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        /* //Однострочная версия if не требует фигурных скобок
+        if (POI == null) return;
+
+        // Получить позицию интересующего объекта
+        Vector3 destination = POI.transform.position; */
+
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+
+
+
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Принудительно установить значение destination.z равным camZ, чтобы
+        // отодвинуть камеру подальше
+        destination.z = camZ;
+        // Поместить камеру в позицию destination
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
